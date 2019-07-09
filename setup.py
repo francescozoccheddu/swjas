8cb2a19d3b54ebb4730b370c5be25a603852d53e
Francesco Zoccheddu <zoccheddu.fra@gmail.com>
2019-07-09 21:41:42 +0200

    Change version to 0.0.6

--- setup.py
+++ setup.py
@@ -15,7 +15,7 @@ with open(_requirements_file) as f:
 setup(
     name='swjas',
     author='Francesco Zoccheddu',
-    version='0.0.5',
+    version='0.0.6',
     description='Simple WSGI JSON API Server',
     long_description=_readme,
     long_description_content_type='text/markdown',
